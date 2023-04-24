29e4e0a672d8389516cd1647c4439b8a45f46c60
Lidia A <lidia.lasy@gmail.com>
2023-04-24 18:29:44 +0200

    Add route and some style structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-function App() {
+import { Route, Routes } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import Home from './views/Home'
+import About from './views/About'
 
+import Topbar from './components/topbar/Topbar'
+
+function App() {
   return (
     <>
-      <h1>My contact App</h1>
+      <header>
+        <h1>Proyecto Gamma</h1>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+
+        <Topbar />
+      </main>
     </>
   )
 }
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,9 @@
+import './Topbar.scss'
+
+export default function Topbar() {
+    return (
+        <>
+            <h1 className="topbar">HOLA</h1>
+        </>
+    )
+}
